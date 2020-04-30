01a6edb49c89233b9e06adcc31f5d992c668eb30
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-30 11:34:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,9 @@
 class Song
   attr_accessor :name, :artist
-  
-  @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
   end
-  
-  def self.all
-    @@all
-  end 
 
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
@@ -25,5 +18,4 @@ class Song
     self.artist = artist
     artist.add_song(self)
   end
-
 end
