a761416496514dbfda32e93de0980dc255255889
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-30 11:23:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,8 @@ class Song
   def self.new_by_filename(filename)
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
-    artist= Artist.find_or_create_by_name(artist_name)
+    artist = self.new(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
     artist.song = self
     
